17aca35d84b8ec3bb97812cac2f625bd6d3538b5
BelikAE <BelikAE@HOME-PC>
2024-07-09 22:26:40 +1000

    Выдача

---
+++ Lab2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число");
+            int countP = 0;
+            int countM = 0;
+            int n;
+            do
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n > 0)
+                    countP += 1;
+                else if (n < 0)
+                    countM += 1;
+
+            } while (n != 0);
+
+            if (countP > countM)
+            {
+                Console.WriteLine("Количество положительных - {0}, больше количества отрицательных - {1}", countP, countM);
+            }
+            else if (countP < countM)
+            {
+                Console.WriteLine("Количество положительных - {0},  меньше количества отрицательных - {1}", countP, countM);
+            }   
+            else
+            {
+                Console.WriteLine("Количество положительных - {0},  равно количества отрицательных - {1}", countP, countM);
+            }
+                
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите положительные числа A, B, C");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            int s = 0;
+            while (a - c >= 0)
+            {
+                a -= c;
+                while (b - c >= 0) 
+                {
+                    b -= c;
+                    ++s;
+                }
+            }
+            Console.WriteLine("Колличество квадратов стороной С: {0}", s);
+        }
+    }
+}
